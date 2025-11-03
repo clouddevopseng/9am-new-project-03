07aa8c3172a2adabf0336a1655ba69c94369276d
venkat <venkat@tcs.com>
2025-11-03 09:24:04 +0530

    9am project

---
+++ src/test/java/_am_new_project/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package _am_new_project.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
